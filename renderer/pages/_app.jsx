c5cebff7c44e5488b075106dd32f63d86df9ad80
Uzair <uzair.muaz3k@gmail.com>
2022-12-25 01:50:07 +0500

    Navbar Fixed

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -3,22 +3,9 @@ import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <div className="h-screen">
-      {Component.name == 'Home' ? (
-        <div></div>
-      ) : Component.name == 'Login' ? (
-        <div></div>
-      ) : (
-        <div className="h-[9%]">
-          <Navbar />
-        </div>
-      )}
-
-      <div className="h-[91%]">
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </div>
   );
 }
